60934910b2a9e85ff3333785851067d349829e63
anansi-1 <anansisime7@gmail.com>
2025-07-16 12:24:53 +0300

    set up the server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/anansi-1/Chirpy
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	const port = "8080"
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Server running on port: %s\n",port)
+	log.Fatal(server.ListenAndServe())
+	
+}
